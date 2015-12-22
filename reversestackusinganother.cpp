8711d93d8448c6860a4e52c7dd6de98222297127
Nikhil <Nikhil12321>
2015-12-22 20:40:59 +0530

    reverse_stack

---
+++ reversestackusinganother.cpp
@@ -0,0 +1,92 @@
+#include<iostream>
+#include<stack>
+
+using namespace std;
+
+
+stack<char> reverse(stack<char> s){
+
+
+    stack<char> temp;
+
+    int count;
+    char d, c;
+
+
+    while(!s.empty()){
+
+
+        c = s.top();
+        s.pop();
+
+        while(!temp.empty()){
+
+
+            d = temp.top();
+            temp.pop();
+            s.push(d);
+            count++;
+        }
+
+        temp.push(c);
+
+        while(count>0){
+
+            d = s.top();
+            s.pop();
+            temp.push(d);
+            count--;
+        }
+    }
+
+    while(!temp.empty()){
+
+
+        c = temp.top();
+        temp.pop();
+        s.push(c);
+
+    }
+
+    return s;
+}
+
+
+
+int main(){
+
+
+
+    stack<char> s;
+    char ch = 'a';
+
+    for(int i=0; i<10; i++)
+        s.push(ch++);
+
+    while(!s.empty()){
+
+        cout<<s.top();
+        s.pop();
+
+    }
+
+    cout<<endl;
+
+    ch = 'a';
+
+     for(int i=0; i<10; i++)
+        s.push(ch++);
+
+
+    stack<char> ans = reverse(s);
+
+    while(!ans.empty()){
+
+        cout<<ans.top();
+        ans.pop();
+
+    }
+
+
+
+}
